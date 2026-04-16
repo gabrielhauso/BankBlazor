74839d188d22590389c156765ca60f7a6f3de876
gabriel hauso <gabrielhauso@gmail.com>
2026-04-16 14:24:16 +0200

    Lagt till customerviewmodel och transactionviewmodel

---
+++ BankBlazorClassLibrary/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,36 @@
+﻿using BankBlazorClassLibrary.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBlazorClassLibrary.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public int CustomerId { get; set; }
+
+        public string Gender { get; set; } = null!;
+
+        public string Givenname { get; set; } = null!;
+
+        public string Surname { get; set; } = null!;
+
+        public string Streetaddress { get; set; } = null!;
+
+        public string City { get; set; } = null!;
+
+        public string Zipcode { get; set; } = null!;
+
+        public string Country { get; set; } = null!;
+
+        public DateOnly? Birthday { get; set; }
+
+        public string? Telephonenumber { get; set; }
+
+        public string? Emailaddress { get; set; }
+
+        public List<AccountDTO> Accounts { get; set; } = new();
+    }
+}
