3e581e9e09b27aca60c7858e281bb0298dbd774c
gabriel hauso <gabrielhauso@gmail.com>
2026-04-16 14:09:07 +0200

    Lagt till DTO för transcations

---
+++ BankBlazorClassLibrary/DTOs/TransactionDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBlazorClassLibrary.DTOs
+{
+    public class TransactionDTO
+    {
+        public int TransactionId { get; set; }
+
+        public int AccountId { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public string Type { get; set; } = null!;
+
+        public string Operation { get; set; } = null!;
+
+        public decimal Amount { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+}
