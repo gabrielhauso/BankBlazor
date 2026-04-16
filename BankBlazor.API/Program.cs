13011e7f755aea8d0a5da1f3a587077af8128105
gabriel hauso <gabrielhauso@gmail.com>
2026-04-16 20:31:12 +0200

    Lagt till CustomerService klass och GetAllcustomers metod och GetCustomer metod.

--- BankBlazor.API/Program.cs
+++ BankBlazor.API/Program.cs
@@ -13,7 +13,8 @@ namespace BankBlazor.API
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<BankBlazorContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -23,7 +24,8 @@ namespace BankBlazor.API
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
