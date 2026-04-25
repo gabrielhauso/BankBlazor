d491e8c3b753b6142ca6fcec696c9e09b99c3dd0
gabriel hauso <gabrielhauso@gmail.com>
2026-04-25 19:30:27 +0200

    Konfigurerade httpsclient med API base URL

--- BankBlazor.Client/Program.cs
+++ BankBlazor.Client/Program.cs
@@ -11,7 +11,13 @@ public class Program
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        var apiBaseUrl = builder.Configuration["ApiBaseUrl"];
+
+        builder.Services.AddScoped(sp => new HttpClient
+        {
+            BaseAddress = new Uri(apiBaseUrl!)
+        });
+
 
         await builder.Build().RunAsync();
     }
