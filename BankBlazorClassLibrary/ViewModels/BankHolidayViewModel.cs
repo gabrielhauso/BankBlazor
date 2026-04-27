e8bce207afee139d68d5894b2eed35db17777454
gabriel hauso <gabrielhauso@gmail.com>
2026-04-27 21:29:45 +0200

    Lagt till scottish bank holiday API

---
+++ BankBlazorClassLibrary/ViewModels/BankHolidayViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBlazorClassLibrary.ViewModels
+{
+    public class BankHolidayViewModel
+    {
+        public string? Title { get; set; }
+        public string? Date { get; set; }
+
+        public BankHolidayViewModel? Scotland { get; set; }
+        public List<BankHolidayViewModel> Events { get; set; } = new();
+    }
+}
