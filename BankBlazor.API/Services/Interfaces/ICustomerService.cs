6f3015c17a3f729c002a573759ac689efd931795
gabriel hauso <gabrielhauso@gmail.com>
2026-04-29 21:10:58 +0200

    Lagt till Customers.razor och gjort paginering

--- BankBlazor.API/Services/Interfaces/ICustomerService.cs
+++ BankBlazor.API/Services/Interfaces/ICustomerService.cs
@@ -1,12 +1,15 @@
 ﻿using BankBlazor.API.Models;
 using BankBlazorClassLibrary.DTOs;
 using BankBlazorClassLibrary.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace BankBlazor.API.Services.Interfaces
 {
     public interface ICustomerService
     {
-        Task<List<Customer>> GetAllCustomers();
+        Task<List<Customer>> GetAllCustomers(int pageNumber, int pageSize);
         Task<Customer?> GetCustomer(int id);
+        Task<int> GetTotalCustomerCount();
+       
     }
 }
